7d7dec0e3c98c9fcc70c4b57e18ee353a99a7058
Natan Amaral <69180967+NatanAmaral@users.noreply.github.com>
2022-05-26 09:15:41 -0300

    Add files via upload

---
+++ 07-Python Errors and Exceptions Handling/02-Pylint Test.py
@@ -0,0 +1,17 @@
+# Testing pylint report scores 
+# indentation/whitespaces/docstrings/tab
+
+'''
+Very Simple Script
+'''
+
+def myfunc():
+    '''
+    A simple function
+    '''
+    first = 1
+    second = 2
+    print(first)
+    print(second)
+
+myfunc()
